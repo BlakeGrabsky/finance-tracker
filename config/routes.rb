9f8297029ed80b7891f9ee438f66e36444ebda55
Blake.Grabsky <Blake.Grabsky@medecision.com>
2019-12-12 16:22:45 -0600

    Search friends form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+	
   root 'welcome#index'
+	
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
 	get 'my_friends', to: "users#my_friends"
+	get 'search_friends', to: "users#search"
+	
   resources :user_stocks, only: [:create, :destroy]
 	resources :users, only: [:show]
 	resources :friendships
+	
 end
